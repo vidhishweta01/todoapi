1d420e0e3d1764e040e604a1d856e1c0f3a974fa
vidhishweta01 <vidhishweta01@gmail.com>
2021-09-16 17:56:26 +0530

    added models item and todo

---
+++ db/migrate/20210916122134_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.boolean :done
+      t.text :description
+      t.string :priority
+      t.references :todo, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
