3b87f74592969394d55b8b2d4d75717a08737410
vidhishweta01 <vidhishweta01@gmail.com>
2021-09-16 18:48:18 +0530

    added tests for validation in model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
   belongs_to :todo
+  validates_presence_of :name, :priority, :done
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 RSpec.describe Todo, type: :model do
   context 'associations' do
     it { should have_many(:items) }
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:created_by) }
   end
 end
