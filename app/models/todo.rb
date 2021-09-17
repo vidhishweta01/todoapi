af8e3d257fd7931b8899d2273f376dc3c1a3ffa9
vidhishweta01 <vidhishweta01@gmail.com>
2021-09-17 15:57:11 +0530

    restore back

--- app/models/todo.rb
+++
@@ -1,4 +0,0 @@
-class Todo < ApplicationRecord
-  has_many :items, dependent: :destroy
-  validates_presence_of :created_by, :title
-end
--- spec/models/todo_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Todo, type: :model do
-  context 'associations' do
-    it { should have_many(:items) }
-    it { should validate_presence_of(:title) }
-    it { should validate_presence_of(:created_by) }
-  end
-end
