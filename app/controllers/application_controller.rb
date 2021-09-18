9ac33f3321fe25d60185c0c81d252a91cc59404e
vidhishweta01 <vidhishweta01@gmail.com>
2021-09-18 21:59:38 +0530

    added user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::API
-  protect_from_forgery with: :null_session
-
   respond_to :json
 
-  before_action :underscore_params!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user
 
--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < Devise::SessionsController
   def create
     user = User.find_by_email(sign_in_params[:email])
-
     if user&.valid_password?(sign_in_params[:password])
       @current_user = user
     else
