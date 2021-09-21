11e69f76c13056a8c4f7422f2dfce1bea9dce211
vidhishweta01 <vidhishweta01@gmail.com>
2021-09-21 20:42:18 +0530

    implemented login registration method in session controller and implemented user auth encrypt and dcrypt in aplication controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_18_113017) do
+ActiveRecord::Schema.define(version: 2021_09_21_140742) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 2021_09_18_113017) do
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.string "created_by"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
