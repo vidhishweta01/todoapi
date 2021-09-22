b076d7e82b0cea637e94186bea0374fbdf699b13
vidhishweta01 <vidhishweta01@gmail.com>
2021-09-22 15:02:52 +0530

    added user test

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :todo
   validates_presence_of :name, :done, :priority
+  validates :name, length: { minimum: 3, maximum: 25, message:
+    'it should be at least 3 character and maximum 25'}, 
 end
