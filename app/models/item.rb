4e39a190e856e504ca28537b48cc56f9abc075bc
vidhishweta01 <vidhishweta01@gmail.com>
2021-09-22 17:51:23 +0530

    added test for model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :todo
-  validates_presence_of :name, :done, :priority
-  validates :name, length: { minimum: 3, maximum: 25, message:
-    'it should be at least 3 character and maximum 25'}, 
+  validates_presence_of :name, :priority, :done
+  validates_length_of :name, minimum: 3, maximum: 25, message: 'it should be at least 3 character and maximum 25'
 end
