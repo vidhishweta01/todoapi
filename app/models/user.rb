fa83e50f302045efcfd8da8d173ff8d3a781c7e0
vidhishweta01 <vidhishweta01@gmail.com>
2021-09-23 16:29:43 +0530

    added error controller main cntroller view

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   validates :email, presence: true, format: { with: /\A(.+)@(.+)\z/, message: "Email invalid"  }, uniqueness:{ case_sensitive: false }, length:{minimum: 10}
   validates :password, presence: true, confirmation: true, length: { within: 6..40 }, on: :create, format:
   {
-    with: /([A-Z]+[a-z]+\d+\D+)/,
+    with: /([a-zA-Z]+\d+\D+)/,
     message: 'must be at least 6 characters and include one number and one letter.',
     multiline: true
   }
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
+  root 'main#index'
   namespace :api do
     namespace :v1 do
       post '/login', to: 'sessions#login'
