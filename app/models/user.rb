81095f0279fefdf23799b62a57ff7afabc5fdada
vidhishweta01 <vidhishweta01@gmail.com>
2021-09-23 12:35:41 +0530

    updates for heroku deployment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
-  # validates :email, uniqueness: true, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  # validates :password, presence: true, confirmation: true, length: { within: 6..40 }, on: :create, format:
-  # {
-  #   with: /^[([a-z]|[A-Z])0-9_-]{6,40}$/,
-  #   message: 'must be at least 6 characters and include one number and one letter.',
-  #   multiline: true
-  # }
+  validates :email, presence: true, format: { with: /\A(.+)@(.+)\z/, message: "Email invalid"  }, uniqueness:{ case_sensitive: false }, length:{minimum: 10}
+  validates :password, presence: true, confirmation: true, length: { within: 6..40 }, on: :create, format:
+  {
+    with: /^[([a-z]|[A-Z])0-9_-]{6,40}$/,
+    message: 'must be at least 6 characters and include one number and one letter.',
+    multiline: true
+  }
 end
