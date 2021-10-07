ba38babf7170e90a7a4c83a107687e680aa14274
vidhishweta01 <vidhishweta01@gmail.com>
2021-10-07 20:21:24 +0530

    edited item controller

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :todo
-  validates_presence_of :name, :priority, :done
+  validates_presence_of :name, :priority
   validates_length_of :name, minimum: 3, maximum: 25, message: 'it should be at least 3 character and maximum 25'
 end
